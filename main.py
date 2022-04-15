4e45cdf8bd62f3a3acc3c9db870687ceeaac3cce
Arthur231456 <artur.231456@gmail.com>
2022-04-15 23:37:55 +0300

    Api test

--- main.py
+++ main.py
@@ -43,5 +43,6 @@ def places():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
+    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
